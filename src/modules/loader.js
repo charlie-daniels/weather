eaeb6d66e90e7664b3682d5e754de9549bb7be13
charlie-daniels <charlied030401@gmail.com>
2022-10-09 18:55:34 +0100

    feat: Add style, refactor: API to modules

--- src/modules/loader.js
+++ src/modules/loader.js
@@ -1,34 +1,25 @@
-async function getWeatherData(lat, lon, key) {
-  const res = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`,
-    { mode: 'cors', method: 'GET' },
-  );
-  return res.json();
-}
-
-async function getLocationInfo(location, key) {
-  const res = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${key}`,
-    { mode: 'cors', method: 'GET' },
-  );
-  return (await res.json())[0];
-}
-
-async function queryWeather(location) {
-  const key = '6c2aaae07b183c9e04e5ac786b6c82d9';
-  const locInfo = await getLocationInfo(location, key);
-  return getWeatherData(locInfo.lat, locInfo.lon, key);
-}
+import weather from './weatherapi';
 
 function addEventListeners() {
   document.getElementById('query').addEventListener('submit', (e) => {
     e.preventDefault();
     const data = new FormData(e.target);
     const location = data.get('location');
-    queryWeather(location).then((res) => {
+    weather.queryWeather(location).then((res) => {
       console.log(res);
-    });
+    })
+      .catch((err) => console.log(err));
   });
 }
 
-addEventListeners();
+function setMobileHeight() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+function init() {
+  addEventListeners();
+  setMobileHeight();
+}
+
+init();
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
+      },
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
